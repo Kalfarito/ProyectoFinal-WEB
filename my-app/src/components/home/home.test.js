0400843d9e1d138b24538b2cae2ddf085219f3c4
slayer020 <140449847+slayer020@users.noreply.github.com>
2023-11-28 18:52:28 -0600

    1er avance
    manuser

--- my-app/src/components/home/home.test.js
+++ my-app/src/components/home/home.test.js
@@ -3,12 +3,3 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Home from './home';
 
-describe('<Home />', () => {
-  test('it should mount', () => {
-    render(<Home />);
-    
-    const home = screen.getByTestId('Home');
-
-    expect(home).toBeInTheDocument();
-  });
-});
